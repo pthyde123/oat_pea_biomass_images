106bf7cb4cff22403d51d6320083f7a983808a4c
serenvillwock <72942237+serenvillwock@users.noreply.github.com>
2023-07-27 14:09:01 -0400

    Fixed yml file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-conda env create --prefix ./conda_env --file conda_env.yml --force
+conda env create --prefix ./conda_env --file conda_env_new.yml --force
 
 conda activate --prefix ./conda_env
+
+ipython kernel install --name "local-venv" --prefix ./conda_env
 
 echo "conda environment setup complete"
