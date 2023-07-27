383d5ba85cd152d19444b0468fd4c8a0ee222de5
serenvillwock <72942237+serenvillwock@users.noreply.github.com>
2023-07-27 16:39:45 -0400

    Updating setup script to activate conda base first

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+conda activate
+
 conda env create --prefix ./conda_env --file conda_env_new.yml --force
 
-conda activate --prefix ./conda_env
+conda activate ./conda_env
 
 ipython kernel install --name "local-venv" --prefix ./conda_env
 
-echo "conda environment setup complete"
+jupyter notebook
